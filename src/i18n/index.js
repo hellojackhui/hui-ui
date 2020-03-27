cdabce6174fdd8ddc480dde9bb767dc68164acde
hellojackhui <huijiawe.i@163.com>
2020-03-27 17:38:00 +0800

    i18n组件基本开发完成

---
+++ src/i18n/index.js
@@ -0,0 +1,26 @@
+import defaultLang from './lang/zh-CN';
+import stringFormat from './format';
+
+let lang = defaultLang;
+
+function use(newlang) {
+  lang = newlang;
+}
+
+function t(path, options) {
+  const array = path.split('.');
+  let _currentlang = lang;
+  for (let i = 0; i < array.length; i++) {
+    let property = array[i];
+    let value = _currentlang[property];
+    if (i === array.length - 1) return stringFormat(value, options);
+    if (!value) return '';
+    _currentlang = value;
+  }
+  return '';
+}
+
+export {
+  use,
+  t,
+}
