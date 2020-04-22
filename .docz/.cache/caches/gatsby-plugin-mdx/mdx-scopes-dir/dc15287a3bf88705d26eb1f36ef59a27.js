1d9d2e50350b503dff439b62c607cabd00152434
hellojackhui <huijiawe.i@163.com>
2020-04-22 16:44:22 +0800

    调整部署文件目录

---
+++ .docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/dc15287a3bf88705d26eb1f36ef59a27.js
@@ -0,0 +1,9 @@
+import { Playground, Props } from 'docz';
+import Checkbox from "../../../../../src/checkbox/index.js";
+import * as React from 'react';
+export default {
+  Playground,
+  Props,
+  Checkbox,
+  React
+};
---
+++ .docz/dist/styles-947156b05a3efb0be44a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],[]]);
+//# sourceMappingURL=styles-947156b05a3efb0be44a.js.map
