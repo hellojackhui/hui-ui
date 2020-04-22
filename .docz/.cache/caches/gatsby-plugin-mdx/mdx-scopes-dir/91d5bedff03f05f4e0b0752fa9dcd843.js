8c15d7207421b030a0864a626f9f0a122b22cd8d
hellojackhui <huijiawe.i@163.com>
2020-04-22 17:02:52 +0800

    netlify deploy test2

--- .docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/91d5bedff03f05f4e0b0752fa9dcd843.js
+++ .docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/91d5bedff03f05f4e0b0752fa9dcd843.js
@@ -1,5 +1,5 @@
 import { Playground, Props } from 'docz';
-import Alert from "../../../../../src/alert/alert";
+import Alert from "../../../../../src/alert/Alert";
 import "../../../../../style/css/font-awesome.css";
 import * as React from 'react';
 export default {
