92620308419c12babe0c8b80aafb279075abe554
hellojackhui <huijiawe.i@163.com>
2020-05-10 23:07:56 +0800

    调整项目结构

--- src/index.js
+++ src/index.js
@@ -1,8 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import Alert from './alert';
+import Badge from './badge';
+import BreadCrumb from './breadcrumb';
+import Button from './button';
+import Card from './card';
+import Checkbox from './checkbox';
+import Collapse from './collapse';
+import ColorPicker from './color-picker';
+import Dropdown from './dropdown';
+import i18n from './i18n';
+import Icon from './icon';
+import Input from './input';
+import InputNumber from './input-number';
+import {Row, Column} from './layout';
+import Loading from './loading';
+import Menu from './menu';
+import Message from './message';
+import Modal from './modal';
+import Notification from './notification';
+import PopOver from './popover';
+import Progress from './progress';
+import Radio from './radio';
+import Rate from './rate';
+import Slider from './slider';
+import Step from './steps';
+import Switch from './switch';
+import Tabs from './tabs';
+import Tag from './tag';
+import Transfer from './transfer';
+import Tree from './tree';
+import Upload from './upload';
+import WaterMask from './watermask';
 import './index.css';
-import Page from './page';
 
-ReactDOM.render(<Page />, document.getElementById('root'));
-
-
+export default {
+  Alert,
+  Badge,
+  BreadCrumb,
+  Button,
+  Card,
+  Checkbox,
+  Collapse,
+  ColorPicker,
+  Dropdown,
+  i18n,
+  Icon,
+  Input,
+  InputNumber,
+  Row, 
+  Column,
+  Loading,
+  Menu,
+  Message,
+  Modal,
+  Notification,
+  PopOver,
+  Progress,
+  Radio,
+  Rate,
+  Slider,
+  Step,
+  Switch,
+  Tabs,
+  Tag,
+  Transfer,
+  Tree,
+  Upload,
+  WaterMask
+}
